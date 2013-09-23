28dc19c34e1cb5cb3b0df2c6e0da59749b48d319
Mohamed Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2013-09-23 16:22:53 +0100

    Add livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,43 @@
-module.exports=function(grunt){
+module.exports = function(grunt) {
 	// Project and task configuration
-
 	grunt.initConfig({
-		pkg:grunt.file.readJSON('package.json'),
-		connect:{
-			server:{
-				options:{
-					keepalive: true,  
-					port:9000,
-					base:'.'
+		pkg: grunt.file.readJSON('package.json'),
+		connect: {
+			server: {
+				options: {
+					keepalive: true,
+					port: 9000,
+					base: '.'
 				}
 			}
 		},
-		less:{
-			compile:{
-				files:{
-					"css/style.css":"less/style.less"
-				}	
+		less: {
+			development: {
+				files: {
+					"css/style.css": "less/style.less"
+				}
 			}
+		},
+		watch: {
+			styles: {
+				files: ['less/**/*.less'],
+				tasks: ['less']
+			},
+			livereload: {
+				options: {
+					livereload: true
+				},
+				files: ['less/**/*'],
+			},
+
 		}
 	});
 
 	// Load plugins
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default',['connect','less']);
+	grunt.registerTask('less', ['watch:styles']);
+	grunt.registerTask('default', ['connect']);
 }
